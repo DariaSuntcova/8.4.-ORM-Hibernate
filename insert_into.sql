6f40cf06003e813e0dde3c323a7542defd551aa3
Daria Suntcova <batareyka23@gmail.com>
2023-05-29 17:58:28 +0300

    add insert_into.sql

---
+++ insert_into.sql
@@ -0,0 +1,5 @@
+INSERT INTO PERSONS (name, surname, age, phone_number, city_of_living)
+values ('Nadin', 'Templ', '31', '+71234567896', 'MOSCOW'),
+       ('Alex', 'Nepal', '25', '+74561237896', 'MOSCOW'),
+       ('Lika', 'Star', '40', '+74561237123', 'LONDON');
+
